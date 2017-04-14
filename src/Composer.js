ebba0fa749733338cdce6c56e872fb89840525e1
Julius Kabangi <kabangijulius@gmail.com>
2017-04-15 01:40:24 +0300

    revert

--- src/Composer.js
+++ src/Composer.js
@@ -46,17 +46,16 @@ export default class Composer extends React.Component {
 const styles = StyleSheet.create({
   textInput: {
     flex: 1,
-    paddingLeft: 10,
+    marginLeft: 10,
     fontSize: 16,
     color:'#13222a',
-    backgroundColor:'#e8f7fd',
     lineHeight: 16,
     fontFamily:'Cabin_Medium',
-    paddingTop: Platform.select({
+    marginTop: Platform.select({
       ios: 6,
       android: 0,
     }),
-    paddingBottom: Platform.select({
+    marginBottom: Platform.select({
       ios: 5,
       android: 3,
     }),
--- src/InputToolbar.js
+++ src/InputToolbar.js
@@ -65,8 +65,8 @@ export default class InputToolbar extends React.Component {
 const styles = StyleSheet.create({
   container: {
     borderTopWidth: StyleSheet.hairlineWidth,
-    borderTopColor: '#d3dbdf',
-    backgroundColor: '#fff',
+    borderTopColor: '#b2b2b2',
+    backgroundColor: '#FFFFFF',
   },
   primary: {
     flexDirection: 'row',
